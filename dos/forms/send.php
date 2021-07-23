7c173ca0b436cf2826761a6ba0e9481083ec0783
Oscar Montufar <87542527+xatruchtech@users.noreply.github.com>
2021-07-23 16:53:03 -0400

    Update send.php

--- dos/forms/send.php
+++ dos/forms/send.php
@@ -2,25 +2,25 @@
 require 'PHPMailer/PHPMailerAutoload.php';
 
 //Create a new PHPMailer instance
-$mail = new PHPMailer();
-$mail->IsSMTP();
+mail = new PHPMailer();
+mail->IsSMTP();
  
 //Configuracion servidor mail
-$mail->From = "xatruchtech@gmail.com"; //remitente
-$mail->SMTPAuth = true;
-$mail->SMTPSecure = 'tls'; //seguridad
-$mail->Host = "smtp.gmail.com"; // servidor smtp
-$mail->Port = 587; //puerto
-$mail->Username ='xatruchtech@gmail.com'; //nombre usuario
-$mail->Password = 'Alimaster91'; //contraseña
+mail->From = "xatruchtech@gmail.com"; //remitente
+mail->SMTPAuth = true;
+mail->SMTPSecure = 'tls'; //seguridad
+mail->Host = "smtp.gmail.com"; // servidor smtp
+mail->Port = 587; //puerto
+mail->Username ='xatruchtech@gmail.com'; //nombre usuario
+mail->Password = 'Alimaster91'; //contraseña
 
 //Agregar destinatario
-$mail->AddAddress($_POST['email']);
-$mail->Subject = $_POST['subject'];
-$mail->Body = $_POST['message'];
+mail->AddAddress($_POST['email']);
+mail->Subject = $_POST['subject'];
+mail->Body = $_POST['message'];
 
 //Avisar si fue enviado o no y dirigir al index
-if ($mail->Send()) {
+if (mail->Send()) {
     echo'<script type="text/javascript">
            alert("Email Sent. Thank you!");
         </script>';
